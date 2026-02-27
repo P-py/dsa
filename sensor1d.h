7a49deb18684419668c16baff02aa224375d77bf
Pedro S Santos <pedrosalviano170@gmail.com>
2026-02-26 21:09:47 -0300

    :sparkles: feat: add new header file for sensor1d

---
+++ sensor1d.h
@@ -0,0 +1,10 @@
+typedef struct {
+	float dados[10];
+	int qtd;
+}
+
+void s_init(Sensor1D* s);
+void s_add(Sensor1D* s, float valor);
+float s_media(const Sensor1D* s);
+float s_max(const Sensor1D* s);
+void s_print(const Sensor1D* s);
